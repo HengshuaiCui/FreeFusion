0e039d268f557d0b2654a31e0535804bbc768d38
HengshuaiCui <1165932886@qq.com>
2025-01-03 23:39:20 +0800

    Infrared and Visible Image Fusion via Cross Reconstruction Learning

--- FreeFusion/data_RGB_whu.py
+++ FreeFusion/data_RGB_whu.py
@@ -1,10 +1,10 @@
 import os
 from dataset_RGB_whu import DataLoaderTrain, DataLoaderTest
 
-def get_training_data(rgb_dir, img_options):
+def get_training_data(rgb_dir):
     assert os.path.exists(rgb_dir)
-    return DataLoaderTrain(rgb_dir, img_options)
+    return DataLoaderTrain(rgb_dir)
 
-def get_test_data(rgb_dir, img_options):
+def get_test_data(rgb_dir):
     assert os.path.exists(rgb_dir)
-    return DataLoaderTest(rgb_dir, img_options)
+    return DataLoaderTest(rgb_dir)
